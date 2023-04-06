96b409deb17d508c64ed6ac046f6baea977b1cad
Alex Scrobot <scrobot91@gmail.com>
2023-04-06 23:31:33 +0200

    some improvement

--- main.py
+++ main.py
@@ -1,27 +1,111 @@
-import sys
-import docx
-import asyncio
+import os
+import re
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout, QFileDialog
 
+import sys
+import asyncio
+from docx import Document
+from tqdm import tqdm
 
-async def format_docx(input_path, output_path):
-    # Открываем файл docx
-    doc = docx.Document(input_path)
 
-    # Проходим по всем параграфам в документе
+async def clean_paragraphs(doc):
+    def process_para(para):
+        # Замена табов на пробелы
+        para = re.sub(r'\t', ' ', para)
+
+        # Замена двойных пробелов после точки на одинарные
+        para = re.sub(r'\.  ', '. ', para)
+
+        # Замена пробелов перед запятыми, точками, тире и двоеточиями на ничего
+        para = re.sub(r'\s([.,:;-])', r'\1', para)
+
+        # Удаление отступов, равных двум табам или более
+        para = re.sub(r'\s{2,}', ' ', para)
+
+        return para.strip()
+
+    paragraphs = []
     for para in doc.paragraphs:
-        # Убираем лишние отступы
-        para.paragraph_format.left_indent = 0
-        para.paragraph_format.right_indent = 0
+        para_text = process_para(para.text)
 
-        # Форматируем абзацы
-        para_format = para.paragraph_format
-        para_format.space_before = docx.shared.Pt(12)
-        para_format.space_after = docx.shared.Pt(12)
-        para_format.line_spacing = docx.shared.Pt(14)
+        # Проверяем, является ли абзац разделителем подглавы
+        if re.match(r'\* \* \*', para_text) or re.match(r'ЧАСТЬ |ГЛАВА ', para_text.upper()):
+            para_text = f"\n{para_text}\n"
+            paragraphs.append(para_text)
+            continue
 
-    # Экспортируем отформатированный текст обратно в .docx
-    doc.save(output_path)
+        # Если текст абзаца не пустой и предыдущий абзац заканчивается на конечный символ предложения
+        if paragraphs and para_text and re.search(r'[.!?]$', paragraphs[-1]):
+            paragraphs.append(para_text)
+        else:
+            # Если это первый абзац или предыдущий абзац не заканчивается на конечный символ предложения, объединяем их
+            if paragraphs:
+                paragraphs[-1] = f"{paragraphs[-1]} {para_text}".strip()
+            else:
+                paragraphs.append(para_text)
+
+    new_doc = Document()
+
+    for para in paragraphs:
+        new_doc.add_paragraph(para.strip())
+
+    return new_doc
+
+
+async def process_chunk(doc, output_filename):
+    cleaned_doc = await clean_paragraphs(doc)
+    cleaned_doc.save(output_filename)
+
+
+def split_document(doc, chunk_size):
+    chunks = []
+    current_chunk = []
+
+    for para in doc.paragraphs:
+        current_chunk.append(para)
+
+        if len(current_chunk) >= chunk_size:
+            chunks.append(current_chunk)
+            current_chunk = []
+
+    if current_chunk:
+        chunks.append(current_chunk)
+
+    return chunks
+
+
+def create_document_from_chunks(chunks):
+    new_doc = Document()
+
+    for chunk in chunks:
+        for para in chunk:
+            new_doc.add_paragraph(para.text)
+
+    return new_doc
+
+
+async def format_docx(input_filename, output_filename):
+    document = Document(input_filename)
+    chunks = split_document(document, chunk_size=100)
+    cleaned_chunks = []
+
+    for i, chunk in enumerate(tqdm(chunks, desc="Processing chunks")):
+        temp_doc = create_document_from_chunks([chunk])
+        cleaned_chunk_filename = f"temp_cleaned_chunk_{i}.docx"
+        await process_chunk(temp_doc, cleaned_chunk_filename)
+        cleaned_chunks.append(Document(cleaned_chunk_filename))
+
+    final_doc = Document()
+
+    for cleaned_chunk in cleaned_chunks:
+        for para in cleaned_chunk.paragraphs:
+            final_doc.add_paragraph(para.text)
+
+    final_doc.save(output_filename)
+
+    # delete temp files
+    for i in range(len(cleaned_chunks)):
+        os.remove(f"temp_cleaned_chunk_{i}.docx")
 
 
 class App(QWidget):
